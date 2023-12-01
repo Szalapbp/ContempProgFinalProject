0c13a14016098d16598f267aa264e037457791f4
bszal <szalapbp@mail.uc.edu>
2023-12-01 18:32:13 -0500

    Add project files.

---
+++ TestProject/TeamMembers.cs
@@ -0,0 +1,11 @@
+﻿namespace TestProject
+{
+    public class TeamMembers
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public DateTime Birthdate { get; set; }
+        public string CollegeProgram { get; set; }
+        public int CollegeYear { get; set; }
+    }
+}
